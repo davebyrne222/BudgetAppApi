6ad88e08d773723c3c81d2697d6ba242e51ffdba
davidbyrne <david.byrne222@gmail.com>
2024-07-04 22:07:36 +0100

    Added connection to RDS db - uses .env to configure source

--- src/main/java/com/dave222/budgetapp/BudgetApplication.java
+++ src/main/java/com/dave222/budgetapp/BudgetApplication.java
@@ -1,5 +1,6 @@
 package com.dave222.budgetapp;
 
+import com.dave222.budgetapp.config.DotenvConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BudgetApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BudgetApplication.class, args);
+		SpringApplication app = new SpringApplication(BudgetApplication.class);
+		app.addInitializers(new DotenvConfig());
+		app.run(args);
 	}
 }
